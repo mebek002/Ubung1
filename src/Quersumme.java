0392c97950d7662e155e6b0835ef262b13c721e9
mebek002 <128830263+mebek002@users.noreply.github.com>
2023-03-25 15:41:26 +0100

    Quersumme berechnen

--- src/Quersumme.java
+++ src/Quersumme.java
@@ -1,2 +1,27 @@
+import java.util.Scanner;
 public class Quersumme {
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Geben Sie eine Zahl ein: ");
+
+        int num = scan.nextInt();
+
+        scan.close();
+
+        System.out.println("Quersumme = " + quersumme(num));
+
+    }
+
+    public static int quersumme(int zahl) {
+
+        if (zahl <= 9) return zahl;
+
+        return zahl%10 + quersumme(zahl/10);
+
+
+    }
+
 }
